dbd77fba6e3ff22eb3530a3fc7db62abf16b496c
xuxife <xuxife@gmail.com>
2025-12-15 12:14:40 +1100

    remove visual/react

--- visual/react/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { NextUIProvider } from '@nextui-org/react';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-    return <NextUIProvider style={{ height: '100%' }}>{children}</NextUIProvider>
-}
